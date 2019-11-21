32cf49f3fc7fbfac478ae4ae69156e2312e0806b
Rushiraj <>
2019-11-21 19:53:37 +0530

    updated after pull

--- gatest/gatest/ViewController.swift
+++ gatest/gatest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //Updated in task1
+        //Update after pull request created
     }
 
 
