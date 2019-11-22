f99a216f11cddf134bc40490b0c01c888cddb970
Rushiraj <>
2019-11-22 13:07:12 +0530

    updated again in develop after feature2

--- gatest/gatest/AppDelegate.swift
+++ gatest/gatest/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     //updted in develop after feature2
 
+    //updted in develop after feature2 again
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
