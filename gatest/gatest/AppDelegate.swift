78676d0fbd584b1fd48cc3721be4489e88ac509a
Rushiraj <>
2019-11-22 13:06:22 +0530

    in develop after feature 2

--- gatest/gatest/AppDelegate.swift
+++ gatest/gatest/AppDelegate.swift
@@ -18,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     //updated in feature1
 
+    //updted in develop after feature2
+
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
